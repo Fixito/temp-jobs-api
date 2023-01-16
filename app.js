adaf91af8eaf214de8133c11b3cb00b0de29113f
Thomas ROBERT <thomas_3004@hotmail.fr>
2023-01-16 21:22:29 +0100

    Add swagger docs

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 require('express-async-errors');
-// librairies de sécurité supplémentaires
+//* librairies de sécurité supplémentaires
 const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
+
+//* Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 const express = require('express');
 const app = express();
@@ -33,6 +38,13 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .send("<h1>Jobs API</h1><a href='/api-docs'>Documentation</a>");
+});
+app.use('./api-docs', swaggerUI.serve, swaggerUI.set(swaggerDocument));
 
 //* routes
 app.use('/api/v1/auth', authRouter);
